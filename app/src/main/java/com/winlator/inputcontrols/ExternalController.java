7e643c4ebe922cac2579c86819ffbc302ca62f44
LUAN228 <luanfghsilverio@gmail.com>
2026-08-15 15:51:40 -0300

    Update ExternalController.java

--- app/src/main/java/com/winlator/inputcontrols/ExternalController.java
+++ app/src/main/java/com/winlator/inputcontrols/ExternalController.java
@@ -149,10 +149,16 @@ public class ExternalController implements GamepadSlot {
         return state;
     }
 
-    @Override
+        @Override
     public GamepadVibration getGamepadVibration() {
         if (vibration == null) {
-            vibration = new GamepadVibration(id != null ? id : "");
+            int devId = getDeviceId();
+            if (devId != -1) {
+                InputDevice device = InputDevice.getDevice(devId);
+                if (device != null) {
+                    vibration = new GamepadVibration(device.getVibrator());
+                }
+            }
         }
         return vibration;
     }
